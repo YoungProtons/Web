d59709a980991790167da4154fefd1634a908cd3
akshi909 <137042410+akshi909@users.noreply.github.com>
2023-08-29 21:08:53 +0530

    Add files via upload

---
+++ YP2/script.js
@@ -0,0 +1,28 @@
+document.getElementById('eventBtn').addEventListener('click', function() {
+            
+  window.location.href = 'Event.html';
+});
+document.getElementById('communityBtn').addEventListener('click', function() {
+  
+  window.location.href = 'community.html';
+});
+document.getElementById('homeBtn').addEventListener('click', function() {
+  
+  window.location.href = 'index.html';
+});
+document.getElementById('aboutBtn').addEventListener('click', function() {
+  
+  window.location.href = 'about.html';
+});
+document.getElementById('SignIn').addEventListener('click', function() {
+  
+  window.location.href = 'https://chat.whatsapp.com/ErHH7G1fcLAKKTIjdvtoAo';
+});
+document.getElementById('start').addEventListener('click', function() {
+  
+  window.location.href = 'https://chat.whatsapp.com/ErHH7G1fcLAKKTIjdvtoAo';
+});
+document.getElementById('Explore').addEventListener('click', function() {
+  
+  window.location.href = 'https://chat.whatsapp.com/ErHH7G1fcLAKKTIjdvtoAo';
+});
